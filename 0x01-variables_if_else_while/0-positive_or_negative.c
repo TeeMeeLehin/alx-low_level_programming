95131dcc93385420ce348f6753a212a4cb3c56ee
TeeMeeLehin <samemerald8@gmail.com>
2023-02-17 14:27:01 -0800

    v

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * This function generates random number btw -RAND_MAX/2 and +RAND_MAX/2, and
+ * prints message to console indicating whether the number is positive,
+ * negative, or zero then returns 0 to indicate that the program has completed
+ * successfully.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 int n;

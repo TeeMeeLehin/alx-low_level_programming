8e4874fc5e80ce8ca21877ae53284db0611f6a78
TeeMeeLehin <samemerald8@gmail.com>
2023-02-28 03:02:28 -0800

    back

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * main - Update the value pointed to by a pointer to 98.
+ *
+ * ptr: Pointer to the int value to be updated.
+ * Return: void
+ */
+int main(void)
+{
+int var, *ptr;
+ptr = &var;
+var = 98;
+}

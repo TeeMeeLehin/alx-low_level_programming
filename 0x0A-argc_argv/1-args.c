366b03bb4ba3b539338e58e3d30294e516d421f6
TeeMeeLehin <samemerald8@gmail.com>
2023-03-10 16:40:08 +0100

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the number of passsed arguments
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}

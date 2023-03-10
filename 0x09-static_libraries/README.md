36b928ba7080a9346ac2e4216862ea10f7b0c093
TeeMeeLehin <samemerald8@gmail.com>
2023-03-10 11:36:34 +0100

    static library

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+folder for the static library projects
---
+++ 0x09-static_libraries/sourcec.c
@@ -0,0 +1,100 @@
+#include "main.h"
+int _putchar(char c) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _islower(int c) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _isalpha(int c) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _abs(int n) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _isupper(int c) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _isdigit(int c) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+int _strlen(char *s) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+void _puts(char *s) {
+    /* TODO: Implement function */
+}
+
+char *_strcpy(char *dest, char *src) {
+    /* TODO: Implement function */
+    return (dest);
+}
+
+int _atoi(char *s) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+char *_strcat(char *dest, char *src) {
+    /* TODO: Implement function */
+    return (dest);
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    /* TODO: Implement function */
+    return (dest);
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    /* TODO: Implement function */
+    return (dest);
+}
+
+int _strcmp(char *s1, char *s2) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    /* TODO: Implement function */
+    return (s);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    /* TODO: Implement function */
+    return (dest);
+}
+
+char *_strchr(char *s, char c) {
+    /* TODO: Implement function */
+    return (NULL);
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    /* TODO: Implement function */
+    return (0);
+}
+
+char *_strpbrk(char *s, char *accept) {
+    /* TODO: Implement function */
+    return (NULL);
+}
+
+char *_strstr(char *haystack, char *needle) {
+    /* TODO: Implement function */
+    return (NULL);
+}
+

f17b86a8880cf065051b08105ca05ba266685779
TeeMeeLehin <samemerald8@gmail.com>
2023-03-10 16:29:03 +0100

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
-* _print_name - prints the name of the program
-* @argc: the number of arguments passed
-* @argv: the array of argument strings passed
-*
-* Return: int
-*/
-int _print_name(int argc, char *argv[])
+ * main - prints the name of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 printf("%s", argv[0]);
 return (0);
 }
-
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-int _print_name(int argc, char *argv[])
+void _print_name(int argc, char *argv[]);
+
 
 #endif /* MAIN_H */

e69c1d2c1899bc63f79c7beeefe8fb066b8336f4
TeeMeeLehin <samemerald8@gmail.com>
2023-03-13 22:30:48 +0100

    intro to malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_array - function to ...wait for it...
+ * create an array lol
+ * @size: size of the array
+ * @c: array init
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (NULL);
+}
+char *str;
+str = malloc(sizeof(char) * size);
+str[0] = c;
+return (*str);
+}

0bfc2f7e6e694f272b8e5ffe03d5fd29907c46bd
TeeMeeLehin <samemerald8@gmail.com>
2023-03-14 22:15:32 +0100

    allocating 2D grid in memory

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+/**
+ * alloc_grid - function to allocate mem for 2D grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: char
+ */
+int **alloc_grid(int width, int height)
+{
+int **grid = malloc(height * sizeof(int));
+int i;
+if (height > 0 && width > 0)
+{
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+}
+return (grid);
+}
+return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define NULL ((void*)0)
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +10,8 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
 

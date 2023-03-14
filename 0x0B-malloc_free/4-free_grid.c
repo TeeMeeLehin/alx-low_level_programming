51eb3eb522449fc56e0856ae5fd689d2119b9f82
TeeMeeLehin <samemerald8@gmail.com>
2023-03-14 22:39:03 +0100

    freeing allocated mem

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - function to  free allocated mem
+ * @grid:  the grid
+ * @height: height of the grid
+ *
+ * Return: char
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(arr[i]);
+}
+}

15463ab74bc013e521de112ccc13199b8518669c
TeeMeeLehin <samemerald8@gmail.com>
2023-03-21 14:59:23 +0100

    declaring struct for a dog instance

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
 * struct dog - an instance of a dog class
 * @name: name of dog
@@ -5,9 +8,10 @@
 * @owner: name of dog's owner
 */
 
-struct dog
-{
-char *name;
-float age;
-char *owner;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
 };
+
+#endif /* DOG_H */

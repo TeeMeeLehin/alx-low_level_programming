0345fc20ae80ffad37f5501eefe662fb825ae266
TeeMeeLehin <samemerald8@gmail.com>
2023-03-22 14:05:09 +0100

    indexing function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,6 +15,8 @@ if (size <= 0)
 {
 return (-1);
 }
+if (array != NULL && cmp != NULL)
+{
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
@@ -25,4 +27,7 @@ break;
 res = -1;
 }
 return (res);
+};
+return (-1);
 }
+

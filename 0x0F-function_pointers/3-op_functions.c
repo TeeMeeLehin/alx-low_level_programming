cc8216ddbb84bd1639dd2e395d5b8362fc7d55f4
TeeMeeLehin <samemerald8@gmail.com>
2023-03-22 21:30:31 +0100

    code module to implement basic math operations using function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+* op_add - addition function
+* @a: first num
+* @b: second num
+* Return: int
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - subtraction function
+* @a: first num
+* @b: second num
+* Return: int
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - multiplication function
+* @a: first num
+* @b: second num
+* Return: int
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - division function
+* @a: first num
+* @b: second num
+* Return: int
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+* op_mod - mod function
+* @a: first num
+* @b: second num
+* Return: int
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}

6c6163b3a13e89b2b3caee24c24c1b73e89fcbb5
TeeMeeLehin <samemerald8@gmail.com>
2023-03-22 12:56:58 +0100

    callback functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
 * print_name - callback function to print given name
 * @name: name char
@@ -8,5 +9,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
+{
 (*f)(name);
+}
 }

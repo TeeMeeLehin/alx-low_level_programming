cc5fc6137230497efdced6d5ade341998de667fc
TeeMeeLehin <samemerald8@gmail.com>
2023-03-22 13:49:55 +0100

    indexing function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,9 +10,11 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (array != NULL && cmp != NULL)
-{
 int i, res;
+if (size <= 0)
+{
+return (-1);
+}
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
@@ -23,6 +25,4 @@ break;
 res = -1;
 }
 return (res);
-}
-return (-1);
 }

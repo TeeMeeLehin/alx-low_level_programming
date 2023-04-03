cf7c9393b91f4efa485e9d09e93d3a9181aaf30a
TeeMeeLehin <samemerald8@gmail.com>
2023-04-04 00:03:20 +0100

    fix attempt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
+* add_nodeint - function to add node to head of linked list
+* @head: the linked list
+* @n: the node element to be added
+* Return: address of new head node
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *neww;
+neww = malloc(sizeof(listint_t));
+if (neww)
+{
+neww->n = n;
+neww->next = *head;
+*head = neww;
+return (*head);
+}
+else
+{
+return (NULL);
+}
+}
+
+/**
 * insert_nodeint_at_index - function to insert node at given index
 * @head: the linked list
 * @idx: index n
@@ -20,6 +43,11 @@ return (NULL);
 temp = *head;
 while (temp)
 {
+if (idx == 0)
+{
+add_nodeint(head, n);
+return (add_nodeint(head, n));
+}
 if (count == idx - 1)
 {
 break;

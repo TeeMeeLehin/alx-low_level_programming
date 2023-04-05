28217d7c9934dd904fa67dcbcd8f3bdae6743f23
TeeMeeLehin <samemerald8@gmail.com>
2023-04-05 22:27:40 +0100

    fixed function for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,29 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-* add_nodeint - function to add node to head of linked list
-* @head: the linked list
-* @n: the node element to be added
-* Return: address of new head node
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-listint_t *neww;
-neww = malloc(sizeof(listint_t));
-if (neww)
-{
-neww->n = n;
-neww->next = *head;
-*head = neww;
-return (*head);
-}
-else
-{
-return (NULL);
-}
-}
-
-/**
 * insert_nodeint_at_index - function to insert node at given index
 * @head: the linked list
 * @idx: index n
@@ -33,25 +10,16 @@ return (NULL);
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *prev, *next, *newn, *temp;
+listint_t *temp, *newnode;
 unsigned int count = 0;
-newn = malloc(sizeof(listint_t));
-if (!newn)
+newnode = malloc(sizeof(listint_t));
+if (!newnode)
 {
 return (NULL);
 }
 temp = *head;
-while (temp)
+while (count < idx - 1)
 {
-if (idx == 0)
-{
-add_nodeint(head, n);
-return (add_nodeint(head, n));
-}
-if (count == idx - 1)
-{
-break;
-}
 temp = temp->next;
 count++;
 }
@@ -59,10 +27,8 @@ if (!temp)
 {
 return (NULL);
 }
-prev = temp;
-next = prev->next;
-newn->n = n;
-newn->next = next;
-prev->next = newn;
-return (newn);
+newnode->n = n;
+newnode->next = temp->next;
+temp->next = newnode;
+return (newnode);
 }

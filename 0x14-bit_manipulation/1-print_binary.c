d2b305ef00dd1a1acc4ab777b09cac49cf03cb13
TeeMeeLehin <samemerald8@gmail.com>
2023-04-08 14:01:19 +0100

    function to cnvert int to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,20 +14,20 @@ while (chkr != 0)
 {
 if (n & chkr)
 {
-printf("%c", '1');
+printf("1");
 flag = 1;
 }
 else
 {
 if (flag)
 {
-printf("%c", '0');
+printf("0");
 }
 }
 chkr >>= 1;
 }
 if (!flag)
 {
-printf("%c", '0');
+printf("0");
 }
 }

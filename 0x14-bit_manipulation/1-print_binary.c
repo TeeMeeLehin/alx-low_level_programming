aba86f40be668434b5fb883bfed0dedb81002d2f
TeeMeeLehin <samemerald8@gmail.com>
2023-04-08 13:55:42 +0100

    function to convert int to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - function to print int as binary
+* @n: the base 10 int
+* Return: void
+*/
+void print_binary(unsigned long int n)
+{
+unsigned int chkr = 2147483648;
+unsigned int flag = 0;
+while (chkr != 0)
+{
+if (n & chkr)
+{
+printf("%c", '1');
+flag = 1;
+}
+else
+{
+if (flag)
+{
+printf("%c", '0');
+}
+}
+chkr >>= 1;
+}
+if (!flag)
+{
+printf("%c", '0');
+}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@ int _power(int n, int exp);
 int _strlen(const char *s);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 
 

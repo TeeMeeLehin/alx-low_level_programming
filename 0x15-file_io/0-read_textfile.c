bb0471b30e6b2145e76147d80e14f12ce28a818f
TeeMeeLehin <samemerald8@gmail.com>
2023-04-10 22:48:38 +0100

    updated read_textfile function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,20 +14,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fp = open(filename, O_RDONLY);
-size_t n, count = 0;
-char *buffer = malloc(letters);
-if (fp == -1 || filename == NULL)
+ssize_t fp, n, count = 0;
+char *buffer;
+if (filename == NULL)
 {
 return (0);
 }
-while ((n = read(fp, buffer, letters)) > 0 && count < letters)
-{
-if ((count + n) > letters)
-{
-n = letters - count;
-}
+fp = open(filename, O_RDONLY);
+buffer = malloc(letters);
+n = read(fp, buffer, letters);
 count += write(1, buffer, n);
+if (fp == -1 || n == -1 || count == -1 || n != count)
+{
+free(buffer);
+return (0);
 }
 return (count);
 }

7c51301e80d5258f0c1fc56bc6845921baddb956
TeeMeeLehin <samemerald8@gmail.com>
2023-04-10 22:33:28 +0100

    updated function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,23 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "main.h"
+
+/**
+* the_closer - error handler for close()
+* @fp: the file pointer
+* Return: void
+*/
+void the_closer(int fp)
+{
+int closer = close(fp);
+if (closer == -1)
+{
+dprintf(2, "Error: Can't close fd %d\n", closer);
+exit(100);
+}
+return;
+}
+
 /**
  * main - copies content of one file to another
  * @argc: the number of arguments passed to the program
@@ -12,8 +29,8 @@
  */
 int main(int argc, char *argv[])
 {
-char *buffer = malloc(1024);
-int fp, des, close1, close2;
+char buffer[1024];
+int fp, des;
 ssize_t r = 0, w;
 if (argc != 3)
 {
@@ -32,21 +49,21 @@ if (des == -1)
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-r = read(fp, buffer, 1024);
+while ((r = read(fp, buffer, 1024)) > 0)
+{
 w = write(des, buffer, r);
 if (w != r)
 {
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-free(buffer);
-close1 = close(fp);
-close2 = close(des);
-if (close1 == -1 || close2 == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", close2);
-exit(100);
 }
+if (r == -1)
+{
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+the_closer(fp);
+the_closer(des);
 return (0);
 }
-

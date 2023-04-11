3461e15b8a90b4c464034a1fb537eedf2b165777
TeeMeeLehin <samemerald8@gmail.com>
2023-04-11 14:27:52 +0100

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* get_endianness - function to determine endianness
+* Return: int
+*/
+
+int get_endianness(void)
+{
+int n = 1;
+char *byte = (char *) &n;
+return ((int) *byte);
+}

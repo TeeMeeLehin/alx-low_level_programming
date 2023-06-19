dfb62f323b3e1273e9518a57803fd6d726910131
TeeMeeLehin <samemerald8@gmail.com>
2023-06-19 21:30:04 +0100

    dynamic library

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,59 @@
+/**
+* add - Function to add two integers.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The sum of the two integers.
+*/
+int add(int a, int b)
+{
+return a + b;
+}
+
+/**
+* sub - Function to subtract two integers.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The result of subtracting the second integer from the first integer.
+*/
+int sub(int a, int b)
+{
+return a - b;
+}
+
+/**
+* mul - Function to multiply two integers.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The product of the two integers.
+*/
+int mul(int a, int b)
+{
+return a * b;
+}
+
+/**
+* div - Function to divide two integers.
+* @a: The dividend.
+* @b: The divisor.
+*
+* Return: The result of dividing the dividend by the divisor.
+*/
+int div(int a, int b)
+{
+return a / b;
+}
+
+/**
+* mod - Function to calculate the modulo of two integers.
+* @a: The dividend.
+* @b: The divisor.
+*
+* Return: The modulo of the dividend and divisor.
+*/
+int mod(int a, int b)
+{
+return a % b;
+}

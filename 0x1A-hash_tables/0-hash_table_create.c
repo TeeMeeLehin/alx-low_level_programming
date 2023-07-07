9eb1039d00f9c591e18c25f430cb894059f0869c
TeeMeeLehin <samemerald8@gmail.com>
2023-07-07 22:31:55 +0100

    C Hash Dict

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
-hash_table_t *table =  malloc(sizeof(hash_table_t));
+hash_table_t *table =  (hash_table_t*)malloc(sizeof(hash_table_t));
 if (!table)
 {
 return (NULL);
 }
 table->size = size;
-table->array = malloc(table->size * sizeof(hash_node_t));
+table->array = (hash_node_t**)malloc(size * sizeof(hash_node_t));
 if (!table->array)
 {
 return (NULL);

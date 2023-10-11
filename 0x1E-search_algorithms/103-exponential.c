aff8096a70426966f86409ff1d2fe5d4686d6c1b
TeeMeeLehin <samemerald8@gmail.com>
2023-10-11 22:41:14 +0100

    Big O

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - function to perform a exponential search
+ * @array: array to be searched
+ * @size: length of the array
+ * @value: value to be sought
+ * Return: index value if successful else -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t stop);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
